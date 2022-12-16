4f8d0445a061cec8c740b8fbf6d9e403eae26293
Nicolas Zozol <nzozol@gmail.com>
2022-12-16 15:40:37 +0100

    Preparing new tests

---
+++ tests/src/add_liquidity.test.js
@@ -0,0 +1,35 @@
+import { processTest, populateTransaction } from "./test.fixture";
+
+const contractName = "Paraswap";
+
+const testLabel = "joinPoolVia0x"; // <= Name of the test
+const testDirSuffix = "join_pool"; // <= directory to compare device snapshots to
+const testNetwork = "polygon";
+const signedPlugin = false;
+
+// Swaap Router
+const contractAddr = "0xe63e0f7526397a2900F870eF9Df6b52CB6330755";
+const chainID = 137;
+
+// From : https://polygonscan.com/tx/0x71ac96610fa24ceca7302453813bb3ae2af9e0f3ceb8897ccef0fe1ec7bbef6c
+const inputData = "0x9d0472e20000000000000000000000007f5f7411c2c7ec60e2db946abbe7dc354254870b0000000000000000000000000000000000000000000000005ff25be9d009cd3a00000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000063999f5600000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000b83e67733bb6280000000000000000000000000000000000000000000000000000000003d42adbc00000000000000000000000000000000000000000000000000000000004260a6";
+
+// Create serializedTx and remove the "0x" prefix
+const serializedTx = populateTransaction(contractAddr, inputData, chainID);
+
+const devices = [
+    {
+        name: "nanos",
+        label: "Nano S",
+        steps: 13, // <= Define the number of steps for this test case and this device
+    },
+    {
+        name: "nanox",
+        label: "Nano X",
+        steps: 7, // <= Define the number of steps for this test case and this device
+    },
+];
+
+devices.forEach((device) =>
+    processTest(device, contractName, testLabel, testDirSuffix, "", signedPlugin, serializedTx, testNetwork)
+);
